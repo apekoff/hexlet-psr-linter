14700a4b62f978c22df673ca13d3b0b190af46a8
apekoff <apekoff@gmail.com>
2016-08-03 17:50:58 +0300

    prepare to step1

--- src/Linter.php
+++ src/Linter.php
@@ -4,8 +4,34 @@ namespace PhpLinter;
 
 class Linter
 {
-    public function linter()
+    public $file;
+    public $line;
+    public $error;
+    public $value;
+
+    public function __construct($file)
     {
-        return true;
+        $this->file = $file;
+    }
+
+    public function parseFile($file)
+    {
+        $this->file = file_get_contents($file);
+        // print_r($this->file);
+        foreach (preg_split("/((\r?\n)|(\r\n?))/", $this->file) as $line) {
+            // print_r($line);
+            if (strpos($line, "function")) {
+                if (preg_match("(function\s(.*)\()", $line, $result)) {
+                    echo ($result[0] . "\n");
+                    // $this->value = $result[0];
+                    // isGoodFunc($this->value);
+                }
+            }
+        }
+    }
+
+    public function isGoodFunc($value)
+    {
+
     }
 }
